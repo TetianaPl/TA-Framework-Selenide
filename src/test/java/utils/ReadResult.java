cbe75844ad7355ed0c1fa654b03041f0072afcc2
Tetiana Plaksina <tetiana_plaksina@epam.com>
2023-09-06 19:36:53 +0200

    Downgraded to Java-11 so that I can run it through Jenkins.

--- src/test/java/utils/ReadResult.java
+++ src/test/java/utils/ReadResult.java
@@ -10,12 +10,17 @@ public class ReadResult {
         if (source.equals("email")) {
             Selenide.executeJavaScript("arguments[0].scrollIntoView(false);", $(By.xpath("//h3[text()='Total Estimated Monthly Cost']")));
         }
-        String result = switch (source) {
-            case "calculatorForm" ->
-                    $(By.xpath("//*[@id='resultBlock']//b[contains(text(), 'Total Estimated Cost:')]")).getText();
-            case "email" -> $(By.xpath("//h2[contains(text(), 'Estimated Monthly Cost:')]")).getText();
-            default -> "";
-        };
+        String result;
+        switch (source) {
+            case "calculatorForm":
+                result = $(By.xpath("//*[@id='resultBlock']//b[contains(text(), 'Total Estimated Cost:')]")).getText();
+                break;
+            case "email":
+                result = $(By.xpath("//h2[contains(text(), 'Estimated Monthly Cost:')]")).getText();
+                break;
+            default:
+                result = "";
+        }
         return Double.parseDouble(result.split(" USD ")[1].
                 split(" per")[0].
                 replaceAll(",", ""));
