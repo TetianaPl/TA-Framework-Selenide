4504923c4228dd3c4fd8400f75bfcb29d55c34f2
Tetiana Plaksina <tetiana_plaksina@epam.com>
2023-09-10 02:57:40 +0200

    Modified so that screenshots are taken using Selenium

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -1,15 +1,43 @@
 package utils;
 
-import com.codeborne.selenide.Configuration;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class TestListener implements ITestListener {
     private final Logger logger = LogManager.getRootLogger();
 
     public void onTestFailure(ITestResult iTestResult) {
-        logger.error("Test failed. Screenshot saved in " + Configuration.reportsFolder);
+        logger.error("Test failed.");
+        saveScreenshot();
+    }
+
+    private void saveScreenshot() {
+        File screenCapture = ((TakesScreenshot)getWebDriver())
+                .getScreenshotAs(OutputType.FILE);
+        try {
+            String pathName = ".//target/screenshots/"
+                    + getCurrentTimeAsString() + ".png";
+            FileUtils.copyFile(screenCapture, new File(pathName));
+            logger.warn("Screenshot saved in " + pathName);
+        } catch (IOException e) {
+            logger.error("Failed to save screenshot: " + e.getLocalizedMessage());
+        }
+    }
+
+    private String getCurrentTimeAsString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd_HH-mm-ss");
+        return ZonedDateTime.now().format(formatter);
     }
 }
